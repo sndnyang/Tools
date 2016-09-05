8b5a8447609e9902819b3f87086954805d9004e3
sndnyang <sndnyangd@gmail.com>
2016-09-05 14:07:09 +0800

    excel版先用， 以后再转 csv

--- revdoc.sh
+++ revdoc.sh
@@ -5,4 +5,4 @@ name=`echo $1 | cut -d "." -f 1`
 
 objname=$name.html
 
-pandoc "$1" -o "$objname"  -t revealjs -s --slide-level=2 -V revealjs-url:"http://cdn.bootcss.com/reveal.js/3.2.0"  --mathjax="//cdn.bootcss.com/mathjax/2.6.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+pandoc "$1" -o "$objname"  -t revealjs -s --slide-level=2 -V revealjs-url:"http://cdn.bootcss.com/reveal.js/3.2.0" --mathjax="http://cdn.bootcss.com/mathjax/2.6.1/MathJax.js?config=TeX-AMS-MML_HTMLorMML" 
