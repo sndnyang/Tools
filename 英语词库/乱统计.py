a8e808e6ee442a1f04e813e2bb57a6dd8354c3d0
sndnyang <sndnyangd@gmail.com>
2016-10-26 19:59:04 +0800

    统计下各字母开头的单词还有多少要编的

---
+++ 英语词库/乱统计.py
@@ -0,0 +1,37 @@
+﻿import sys
+import xlrd
+import numpy as np
+import pandas as pd
+
+data = xlrd.open_workbook(sys.argv[1])
+table = data.sheets()[0]
+nrows = table.nrows # 获取表的行数
+l = ['word', 'level', 'lenovo', 'etyma', 'meanZh', 'meanEn', 'example']
+
+content = []
+
+for i in range(2, nrows): # 循环逐行打印
+    if i == 0: # 跳过第一行
+        continue
+    row = table.row_values(i)[:len(l)]
+    if len(row) > 7:
+        print len(row), row
+        sys.exit()
+    content.append(row)
+
+print len(content)
+
+data = pd.DataFrame(content, columns=l)
+
+tmp = data[data['level'] <= 0.2]
+
+a = pd.Series([e[0] for e in tmp['word']])
+
+t = pd.DataFrame({'c': a})
+
+t['a'] = 1
+
+print t.groupby('c').count().sort_values('a')
+
+#print data[:3]
+
