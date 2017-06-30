71292d033adf086d4cb8a89b2d0c1b315a8ac9b6
sndnyang <sndnyangd@gmail.com>
2017-07-01 00:27:00 +0800

    拆分总库

--- 英语词库/xls2db.py
+++ 英语词库/xls2db.py
@@ -1,37 +1,73 @@
-﻿import sys
+﻿import time
+import os.path
+
+import sys
 import xlrd
 import numpy as np
 import pandas as pd
 import sqlite3 as sq
 
-data = xlrd.open_workbook(sys.argv[1])
-table = data.sheets()[0]
-nrows = table.nrows # 获取表的行数
-l = ['word', 'level', 'lenovo', 'etyma', 'meanZh', 'meanEn', 'example']
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-content = []
 
-for i in range(2, nrows): # 循环逐行打印
-    if i == 0: # 跳过第一行
-        continue
-    row = table.row_values(i)[:len(l)]
-    if len(row) > 7:
-        print len(row), row
-        sys.exit()
-    content.append(row)
+def xls2list(fname):
 
-print len(content)
+    data = xlrd.open_workbook(fname + '.xls')
+    table = data.sheets()[0]
+    nrows = table.nrows # 获取表的行数
+    l = ['word', 'level', 'lenovo', 'etyma', 'meanZh', 'meanEn', 'example', 'phonetic', 'test']
 
-data = pd.DataFrame(content, columns=l)
+    content = []
 
-data['selfLenovo'] = ""
+    for i in range(1, nrows): # 循环逐行打印
+        if i == 0: # 跳过第一行
+            continue
+        row = table.row_values(i)[:len(l)]
+        if len(row) > len(l):
+            print len(row), row
+            sys.exit()
+        content.append(row)
 
-#data = pd.read_csv(sys.argv[1] + '.csv', index_col=False)
+    print len(content)
+    return content, l
 
-data['level'] = 0
+def list2df(para):
+    content, l = para
+    data = pd.DataFrame(content, columns=l)
 
-#print data[:3]
+    data['selfLenovo'] = ""
+    data['level'] = 0
+    return data
 
-con  = sq.connect(sys.argv[2] + '.db')
+def gloss2test(data):
 
-data.to_sql('word', con)
+    test = {'CET4':'四级', 'CET6':'六级', 'TOFEL':'托福', 
+            'GRE':'GRE', 'GRE3000':'GRE-再要你命3000', 
+            'GREmagoosh':'GRE-Magoosh'}
+
+    f = file("books.txt", "w")
+
+    for t in test:
+        if t == 'CET6':
+            subset = data[(data['test'].str.contains(t))&(~data['test'].str.contains('CET4'))]
+        else:
+            subset = data[(data['test'].str.contains(t))]
+
+        print t, len(subset)
+        con  = sq.connect(t.lower() + '.db')
+        subset[subset.columns[:-1]].to_sql('word', con)
+        con.close()
+
+        m = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime(os.path.getmtime('gloss.xls')))
+        s = "%s http://7xt8es.com1.z0.glb.clouddn.com/naodong/word/%s.db %d(%s更新)" % (test[t], t.lower(), len(subset), m)
+
+        f.write(s)
+
+        f.write('\n')
+
+    f.close()
+
+
+if __name__ == '__main__':
+    gloss2test(list2df(xls2list('gloss3')))
