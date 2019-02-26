0bd82e76e3dbd64e6e8876f1be77583830cefdc5
Xiulong Yang <xyang22@student.gsu.edu>
2019-02-26 16:41:57 -0500

    update tool

--- research.sh/clean_results.sh
+++ research.sh/clean_results.sh
@@ -5,19 +5,29 @@ do
     echo $pid;
     result_d=`find ~/project/results -name "*$marker\_*" -type d`
     echo "path is " $result_d
-    read -p "do you want to remove the directory ?(y/n)" input
-    case $input in
-        [yY][eE][sS]|[yY])
-            mv "$result_d" ~/tmp_as_deletes
-        ;;
-    esac
+    if [[ -d "$result_d" ]]
+    then
+        read -p "do you want to remove the directory ?(y/n)" input
+        case $input in
+            [yY][eE][sS]|[yY])
+                mv "$result_d" ~/tmp_as_deletes
+            ;;
+        esac
+    else
+        echo "not find it"
+    fi
 
-    tf_d=`find ~/project/runs -name "*$marker\_*" -type d`
+    tf_d=`find ~/project/runs -name "*$marker*" -type d`
     echo "path is " $tf_d
-    read -p "do you want to remove the directory ?(y/n)" input
-    case $input in
-        [yY][eE][sS]|[yY])
-            mv "$tf_d" ~/tmp_as_deletes
-        ;;
-    esac
+    if [[ -d "$result_d" ]]
+    then
+        read -p "do you want to remove the directory ?(y/n)" input
+        case $input in
+            [yY][eE][sS]|[yY])
+                mv "$tf_d" ~/tmp_as_deletes
+            ;;
+        esac
+    else
+        echo "not find it"
+    fi
 done
